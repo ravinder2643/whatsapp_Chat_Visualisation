5002ce3fcd18643517adbfc759d25b5582505e16
ravinder2643 <138236210+ravinder2643@users.noreply.github.com>
2024-04-04 21:20:26 +0530

    new commit

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import streamlit as st
-import preprocessor,helper
+import preprocessor
+import helper
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-
-
-
 
 st.sidebar.title("Whatsapp Chat Analyzer")
 
@@ -17,18 +15,18 @@ if uploaded_file is not None:
 
     # fetch unique users
     user_list = df['user'].unique().tolist()
-    
+
     if 'group_notification' in user_list:
         user_list.remove('group_notification')
     user_list.sort()
-    user_list.insert(0,"Overall")
+    user_list.insert(0, "Overall")
 
-    selected_user = st.sidebar.selectbox("Show analysis wrt",user_list)
+    selected_user = st.sidebar.selectbox("Show analysis wrt", user_list)
 
     if st.sidebar.button("Show Analysis"):
 
         # Stats Area
-        num_messages, words, num_media_messages, num_links = helper.fetch_stats(selected_user,df)
+        num_messages, words, num_media_messages, num_links = helper.fetch_stats(selected_user, df)
         st.title("Top Statistics")
         col1, col2, col3, col4 = st.columns(4)
 
@@ -47,9 +45,9 @@ if uploaded_file is not None:
 
         # monthly timeline
         st.title("Monthly Timeline")
-        timeline = helper.monthly_timeline(selected_user,df)
-        fig,ax = plt.subplots()
-        ax.plot(timeline['time'], timeline['message'],color='green')
+        timeline = helper.monthly_timeline(selected_user, df)
+        fig, ax = plt.subplots()
+        ax.plot(timeline['time'], timeline['message'], color='green')
         plt.xticks(rotation='vertical')
         st.pyplot(fig)
 
@@ -63,13 +61,13 @@ if uploaded_file is not None:
 
         # activity map
         st.title('Activity Map')
-        col1,col2 = st.columns(2)
+        col1, col2 = st.columns(2)
 
         with col1:
             st.header("Most busy day")
-            busy_day = helper.week_activity_map(selected_user,df)
-            fig,ax = plt.subplots()
-            ax.bar(busy_day.index,busy_day.values,color='purple')
+            busy_day = helper.week_activity_map(selected_user, df)
+            fig, ax = plt.subplots()
+            ax.bar(busy_day.index, busy_day.values, color='purple')
             plt.xticks(rotation='vertical')
             st.pyplot(fig)
 
@@ -77,18 +75,18 @@ if uploaded_file is not None:
             st.header("Most busy month")
             busy_month = helper.month_activity_map(selected_user, df)
             fig, ax = plt.subplots()
-            ax.bar(busy_month.index, busy_month.values,color='orange')
+            ax.bar(busy_month.index, busy_month.values, color='orange')
             plt.xticks(rotation='vertical')
             st.pyplot(fig)
 
         st.title("Weekly Activity Map")
-        user_heatmap = helper.activity_heatmap(selected_user,df)
-        fig,ax = plt.subplots()
-        
+        user_heatmap = helper.activity_heatmap(selected_user, df)
+        fig, ax = plt.subplots()
+
         # Replace NaN values with 0
         user_heatmap = np.nan_to_num(user_heatmap, nan=0)
 
-# Create the heatmap
+        # Create the heatmap
         ax = sns.heatmap(user_heatmap)
 
         st.pyplot(fig)
@@ -96,13 +94,13 @@ if uploaded_file is not None:
         # finding the busiest users in the group(Group level)
         if selected_user == 'Overall':
             st.title('Most Busy Users')
-            x,new_df = helper.most_busy_users(df)
+            x, new_df = helper.most_busy_users(df)
             fig, ax = plt.subplots()
 
             col1, col2 = st.columns(2)
 
             with col1:
-                ax.bar(x.index, x.values,color='red')
+                ax.bar(x.index, x.values, color='red')
                 plt.xticks(rotation='vertical')
                 st.pyplot(fig)
             with col2:
@@ -110,42 +108,31 @@ if uploaded_file is not None:
 
         # WordCloud
         st.title("Wordcloud")
-        df_wc = helper.create_wordcloud(selected_user,df)
-        fig,ax = plt.subplots()
+        df_wc = helper.create_wordcloud(selected_user, df)
+        fig, ax = plt.subplots()
         ax.imshow(df_wc)
         st.pyplot(fig)
 
         # most common words
-        most_common_df = helper.most_common_words(selected_user,df)
+        most_common_df = helper.most_common_words(selected_user, df)
 
-        fig,ax = plt.subplots()
+        fig, ax = plt.subplots()
 
-        ax.barh(most_common_df[0],most_common_df[1])
+        ax.barh(most_common_df[0], most_common_df[1])
         plt.xticks(rotation='vertical')
 
         st.title('Most commmon words')
         st.pyplot(fig)
 
         # emoji analysis
-        emoji_df = helper.emoji_helper(selected_user,df)
+        emoji_df = helper.emoji_helper(selected_user, df)
         st.title("Emoji Analysis")
 
-        col1,col2 = st.columns(2)
+        col1, col2 = st.columns(2)
 
         with col1:
             st.dataframe(emoji_df)
         with col2:
-            fig,ax = plt.subplots()
-            ax.pie(emoji_df[1].head(),labels=emoji_df[0].head(),autopct="%0.2f")
+            fig, ax = plt.subplots()
+            ax.pie(emoji_df[1].head(), labels=emoji_df[0].head(), autopct="%0.2f")
             st.pyplot(fig)
-
-
-
-
-
-
-
-
-
-
-
--- preprocessor.py
+++ preprocessor.py
@@ -2,14 +2,14 @@ import re
 import pandas as pd
 
 def preprocess(data):
-    pattern = '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
+    pattern = r'\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s(?:AM|PM)\s-\s'  # Updated pattern for 12-hour format
 
     messages = re.split(pattern, data)[1:]
     dates = re.findall(pattern, data)
 
     df = pd.DataFrame({'user_message': messages, 'message_date': dates})
     # convert message_date type
-    df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%Y, %H:%M - ')
+    df['message_date'] = pd.to_datetime(df['message_date'], format='%m/%d/%y, %I:%M %p - ')  # Adjusted format for two-digit year
 
     df.rename(columns={'message_date': 'date'}, inplace=True)
 
@@ -34,17 +34,21 @@ def preprocess(data):
     df['month'] = df['date'].dt.month_name()
     df['day'] = df['date'].dt.day
     df['day_name'] = df['date'].dt.day_name()
-    df['hour'] = df['date'].dt.hour
+    
+    # Convert hour to 12-hour format with AM/PM
+    df['hour'] = df['date'].dt.strftime('%I')
+    df['am_pm'] = df['date'].dt.strftime('%p')
+    
     df['minute'] = df['date'].dt.minute
 
     period = []
     for hour in df[['day_name', 'hour']]['hour']:
-        if hour == 23:
-            period.append(str(hour) + "-" + str('00'))
-        elif hour == 0:
-            period.append(str('00') + "-" + str(hour + 1))
+        if hour == '11':
+            period.append('11 PM - 12 AM')
+        elif hour == '12':
+            period.append('12 PM - 01 AM')
         else:
-            period.append(str(hour) + "-" + str(hour + 1))
+            period.append(hour + ' ' + df['am_pm'] + ' - ' + str(int(hour) + 1) + ' ' + df['am_pm'])
 
     df['period'] = period
 
